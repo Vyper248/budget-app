418502e8e5849a9bd0f9e67f4703b155b74d8e72
Vyper248 <vyper375@hotmail.com>
2020-07-21 11:15:52 +0100

    Add option to Grid for start and end columns

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,8 @@ const Grid = styled.div`
     grid-template-columns: ${props => props.template ? props.template : '1fr 1fr'};
     grid-gap: 10px;
     margin: 10px;
+    ${props => props.start !== undefined ? 'grid-column-start:'+props.start : ''};
+    ${props => props.end !== undefined ? 'grid-column-end:'+props.end : ''};
 `;
 
 export default Grid;
