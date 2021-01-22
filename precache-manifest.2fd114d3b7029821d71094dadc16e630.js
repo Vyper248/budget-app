126dff397e4c07dfafffd0146cab342e2ad3967a
Vyper248 <vyper375@hotmail.com>
2021-01-22 12:51:07 +0000

    Updates

---
+++ precache-manifest.2fd114d3b7029821d71094dadc16e630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4ee85358dbc136df0518be105ecb35",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "6c9d87268063b7deae99",
+    "url": "/budget-app/static/css/main.7e8b7f7b.chunk.css"
+  },
+  {
+    "revision": "01fa411c944666f90d01",
+    "url": "/budget-app/static/js/2.b31cc673.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/budget-app/static/js/2.b31cc673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c9d87268063b7deae99",
+    "url": "/budget-app/static/js/main.64e91a03.chunk.js"
+  },
+  {
+    "revision": "98bcc7c1f9780714c7d8",
+    "url": "/budget-app/static/js/runtime-main.bb5cb9cb.js"
+  }
+]);
